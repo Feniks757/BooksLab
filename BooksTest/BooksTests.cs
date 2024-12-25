22d4d6cf4d719e03839dd52138229b4b77317d14
Feniks7575 <ustinafenina@gmail.com>
2024-12-25 19:30:41 +0300

    Add Tests

---
+++ BooksTest/BooksTests.cs
@@ -0,0 +1,11 @@
+namespace BooksTest;
+
+[TestFixture]
+public class BooksTests
+{
+    [Test]
+    public void MyTest()
+    {
+        Assert.Pass();
+    }
+}
---
+++ BooksTest/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using NUnit.Framework;
+global using BooksLab.Books;
+global using BooksLab.Functions;
+global using BooksLab.Interface;
+global using BooksLab.ConsoleCommands;
+global using BooksLab.Output;
