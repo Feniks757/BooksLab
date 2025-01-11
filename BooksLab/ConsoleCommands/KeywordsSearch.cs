5e06defad0e70d7c87313bcd044c5dc9f3741b48
BestAlice <dima_goloskok@lenta.ru>
2025-01-11 03:49:23 +0300

    working version

--- BooksLab/ConsoleCommands/KeywordsSearch.cs
+++ BooksLab/ConsoleCommands/KeywordsSearch.cs
@@ -15,17 +15,28 @@ internal class KeywordSearch : IBookSearch
             .ToArray();
         List<Book> books;
         
-        await using(BookCatalog catalog = new(userId))
+        /*await using(BookCatalog catalog = new(userId))
         {
             books = await catalog.Books.ToListAsync();
+        }*/
+        
+        await using (BookCatalog catalog = new(userId))
+        {
+            return catalog.Books
+                .AsEnumerable()
+                .Where(book =>
+                    keywords.Any(keyword => book.Annotation.Contains(keyword) ||
+                                            book.Genres.Any(genre => genre.ToLower().Contains(keyword))))
+                .ToList();
         }
+        
 
-        var filteredBooks = books
+        /*var filteredBooks = books
             .Where(book =>
                 keywords.Any(keyword => book.Annotation.Contains(keyword.ToLower()) ||
                                         book.Genres.Any(genre => genre.ToLower().Contains(keyword))))
             .ToList();
  
-        return filteredBooks;
+        return filteredBooks;*/
     }
 }
