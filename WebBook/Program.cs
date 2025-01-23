6dc925c4e5812b9cf2eadf747def55c2255369be
BestAlice <dima_goloskok@lenta.ru>
2025-01-23 09:34:20 +0300

    Move configuration in Main

--- WebBook/Program.cs
+++ WebBook/Program.cs
@@ -1,14 +1,35 @@
 using BooksLab;
+using BooksLab.Books;
+using Microsoft.EntityFrameworkCore;
+using WebBook.Services;
 
 namespace WebBook;
 class Program
 {
     public static void Main(string[] args)
     {
-        Host.CreateDefaultBuilder()
-            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-            .Build()
-            .Run();
+        var builder = WebApplication.CreateBuilder(args);
+        string con = "server=localhost;user=root;password=password;database=books;";
+        var version = new MySqlServerVersion(new Version(8, 0, 11));
+        builder.Services.AddDbContextFactory<BookContext>(options => options.UseMySql(con, version));
+
+        builder.Services.AddScoped<IBookService, BookService>();
+        builder.Services.AddControllers();
+        
+
+        var app = builder.Build();
+        app.UseDeveloperExceptionPage();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseDefaultFiles();
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+        
+        app.Run();
     }
 }
     
